0d8e0e0b41ab6930f4ffc9633a8a3a13c1cecc4d
Rutuja <rutujadarade2005@gmail.com>
2026-06-10 21:40:38 +0530

    Allow Cloudinary images and add ingest rewrites
    Permit images from res.cloudinary.com via images.remotePatterns, add rewrites to proxy /ingest/static/:path* -> http://us-assets.i.posthog.com/static/:path* and /ingest/:path* -> http://us.i.posthog.com/:path*, and enable skipTrailingSlashRedirect to avoid automatic trailing-slash redirects. These changes allow loading external Cloudinary assets and route ingest/asset requests to PostHog endpoints.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      }
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "http://us-assets.i.posthog.com/static/:path*"
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "http://us.i.posthog.com/:path*"
+      },
+    ];
+  },
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
