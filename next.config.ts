a6606917705c0973051b6868be51e6ecd6373451
Rutuja <rutujadarade2005@gmail.com>
2026-06-11 13:15:51 +0530

    Update remote image host and remove rewrites
    Change Next.js images.remotePatterns to allow images.unsplash.com instead of res.cloudinary.com. Remove custom rewrites that proxied /ingest and /ingest/static to PostHog endpoints and drop the skipTrailingSlashRedirect setting. Minor formatting/quote adjustments.

--- next.config.ts
+++ next.config.ts
@@ -4,24 +4,11 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      }
-    ]
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/ingest/static/:path*",
-        destination: "http://us-assets.i.posthog.com/static/:path*"
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
-      {
-        source: "/ingest/:path*",
-        destination: "http://us.i.posthog.com/:path*"
-      },
-    ];
+    ],
   },
-  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
