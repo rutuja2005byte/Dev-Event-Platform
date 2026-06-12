f9cf193d32f229c67eeeeb3778687bcd62672aa8
Rutuja <rutujadarade2005@gmail.com>
2026-06-12 19:03:21 +0530

    Make BookEvent a client component
    Enable client-side behavior in components/BookEvent.tsx by adding 'use client' and importing useState, the createBooking action, and posthog for analytics. Prepares the component for client-side booking logic and telemetry.

--- components/BookEvent.tsx
+++ components/BookEvent.tsx
@@ -1,1 +1,24 @@
+'use client';
+
+import {useState} from "react";
+import {createBooking} from "@/lib/actions/booking.actions";
+import posthog from "posthog-js";
+
+const BookEvent = ({ eventId, slug }: { eventId: string, slug: string;}) => {
+    const [email, setEmail] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const { success } = await createBooking({ eventId, slug, email });
+
+        if(success) {
+            setSubmitted(true);
+            posthog.capture('event_booked', { eventId, slug, email })
+        } else {
+            console.error('Booking creation failed')
+            posthog.captureException('Booking creation failed')
+        }
+    }
 
